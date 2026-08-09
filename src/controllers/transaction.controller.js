33ab547c45444a1c46932cdffd659f93ab3bc641
Tanmay745 <tanmaykodnani547@gmail.com>
2026-08-09 13:53:39 +0530

    Refined transaction controller

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -18,10 +18,45 @@ async function createTransaction(req,res){
     const toUserAccount = await accountModel.findOne({
         _id:toAccount
     })
-    
+
     if(!fromUserAccount || !toUserAccount){
         return res.status(400).json({
             message:"Invalid fromAccount or toAccount"
+        })
+    }
+
+
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey:idempotencyKey
+    })
+    if(isTransactionAlreadyExists){
+        if(isTransactionAlreadyExists.status === "COMPLETED"){
+            return res.status(200).json({
+                message:"Transaction already processed",
+                transaction:isTransactionAlreadyExists
+            })
+        }
+        if(isTransactionAlreadyExists === "PENDING"){
+            return res.status(200).json({
+                message:"Transaction is still processing"
+            })
+        }
+        if(isTransactionAlreadyExists === "FAILED"){
+            return res.status(500).json({
+                message:"Transaction processing failed, please retry"
+            })
+        }
+        if(isTransactionAlreadyExists === "REVERSED"){
+            return res.status(500).json({
+                message:"Transaction was reversed, please retry"
+            })
+        }
+    }
+
+
+    if(fromUserAccount.status !== "ACTIVE" || toUserAccount.status !== "ACTIVE"){
+        return res.status(400).json({
+            message:"Both fromAccount and toAccount must be ACTIVE to process transaction"
         })
     }
 }
