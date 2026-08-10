983db4baf1fd76b8c1f1063b8aadfed3b5530ce6
Tanmay745 <tanmaykodnani547@gmail.com>
2026-08-10 23:20:43 +0530

    Added a tokenBlacklist schema to store tokens when a user logs out, preventing their reuse for authentication. Added blacklist validation to reject requests made with tokens that exist in the blacklist

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
-
+const tokenBlacklistModel = require("../models/blacklist.model")
 
 async function authMiddleware(req,res,next){
     const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
@@ -8,6 +8,13 @@ async function authMiddleware(req,res,next){
     if(!token){
         return res.status(401).json({
             message:"Unauthorized access, token is missing"
+        })
+    }
+
+    const isBlacklisted = await tokenBlacklistModel.findOne({token})
+    if(isBlacklisted){
+        return res.status(401).json({
+            message:"Unauthorized access, token is invalid"
         })
     }
 
@@ -29,6 +36,13 @@ async function authSystemUserMiddleware(req,res,next){
     if(!token){
         return res.status(401).json({
             message:"Unauthorized access, token is missing"
+        })
+    }
+
+    const isBlacklisted = await tokenBlacklistModel.findOne({token})
+    if(isBlacklisted){
+        return res.status(401).json({
+            message:"Unauthorized access, token is invalid"
         })
     }
 
